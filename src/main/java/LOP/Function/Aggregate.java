a7f0b089c841a5a56fa8f8bc1944c321cab7ae27
machkouroke <machkour20.mo1@gmail.com>
2022-12-06 16:03:38 +0100

    `feat` Add of Aggregate function

--- src/main/java/LOP/Function/Aggregate.java
+++
@@ -1,8 +0,0 @@
-package LOP.Function;
-
-public enum Aggregate {
-    min(5), max(5), produit(5), somme(5), moyenne(5), logiqueEntree(5);
-
-    Aggregate(Object i) {
-    }
-}
---
+++ src/main/java/LOP/Function/Aggregate/Aggregate.java
@@ -0,0 +1,10 @@
+package LOP.Function.Aggregate;
+
+import LOP.utilities.Matrix;
+
+abstract public class Aggregate {
+    private Matrix matrix;
+    Aggregate(Matrix matrix) {
+        this.matrix = matrix;
+    }
+}
