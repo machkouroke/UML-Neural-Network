4b6059e18fde5edf1a69b20c2c8eba86c3111781
machkouroke <machkour20.mo1@gmail.com>
2022-12-06 20:11:58 +0100

    `feat` Add of test in main for neural network

--- src/main/java/LOP/Main.java
+++ src/main/java/LOP/Main.java
@@ -1,8 +1,25 @@
 package LOP;
 
-;
+
+import LOP.Neural.MLPerceptron;
+import LOP.utilities.Matrix;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[][] X = {
+                {0, 0},
+                {1, 0},
+                {0, 1},
+                {1, 1}
+        };
+        double[][] Y = {
+                {0}, {1}, {1}, {0}
+        };
+        MLPerceptron nn = new MLPerceptron(1, 2, 10, 1);
+        nn.fit(new Matrix(X), new Matrix(Y), 50000);
+        double[][] input = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        for (double[] d : input) {
+            System.out.println(nn.predict(d).toString());
+        }
     }
 }
