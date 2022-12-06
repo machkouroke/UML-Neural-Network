fc6aca768df230163a27e2fafa6f7e72c99a5d0b
machkouroke <machkour20.mo1@gmail.com>
2022-12-06 11:17:59 +0100

    `feat` Renaming package into lop

---
+++ src/main/java/lop/Currency.java
@@ -0,0 +1,7 @@
+package lop;
+
+public enum Currency {PENNY(4), NICKLE(5), DIME(4), QUARTER(8);
+
+    Currency(Object i) {
+    }
+}
---
+++ src/main/java/lop/Neural/NeuralNetwork.java
@@ -0,0 +1,4 @@
+package lop.Neural;
+
+public class NeuralNetwork {
+}
