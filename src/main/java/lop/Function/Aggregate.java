ab61668b223e636450601f3d14d32c9e371fd18d
MorelK1 <kouhossounon.vianney@gmail.com>
2022-12-06 11:40:08 +0100

    `feat` create functions enumerations

---
+++ src/main/java/lop/Function/Aggregate.java
@@ -0,0 +1,8 @@
+package lop.Function;
+
+public enum Aggregate {
+    min(5), max(5), produit(5), somme(5), moyenne(5), logiqueEntree(5);
+
+    Aggregate(Object i) {
+    }
+}
---
+++ src/main/java/lop/Function/Transfert.java
@@ -0,0 +1,8 @@
+package lop.Function;
+
+public enum Transfert {
+
+    linear(2), Heaviside(2), sigmoid(2);
+    Transfert(Object i) {
+    }
+}
