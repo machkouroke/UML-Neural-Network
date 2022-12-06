1d8fbaf06802316b739c2c7ef91281c9416b1a96
machkouroke <machkour20.mo1@gmail.com>
2022-12-06 16:04:16 +0100

    `feat` Connection of Neural and layer

--- src/main/java/LOP/Neural/Layer.java
+++ src/main/java/LOP/Neural/Layer.java
@@ -1,9 +1,24 @@
 package LOP.Neural;
 
+import LOP.Function.Aggregate.Aggregate;
+import LOP.Function.Transfert.Transfert;
+import LOP.utilities.Matrix;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Layer {
     private int neuroneNumber;
+    private Matrix bias;
+    private List<Neural> neurones;
 
-    Layer(int neuroneNumber) {
+    Layer(int neuroneNumber, Matrix bias, Transfert transfertFunc, Aggregate aggregateFunc) {
         this.neuroneNumber = neuroneNumber;
+        this.bias = bias;
+        this.neurones = IntStream.of(neuroneNumber)
+                .mapToObj(i -> new Neural(bias.getData()[i][0], aggregateFunc, transfertFunc))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/LOP/Neural/Neural.java
+++ src/main/java/LOP/Neural/Neural.java
@@ -1,14 +1,14 @@
 package LOP.Neural;
 
-import LOP.Function.Aggregate;
+import LOP.Function.Aggregate.Aggregate;
 import LOP.Function.Transfert.Transfert;
 
 public class Neural {
-    private float weight;
+    private double bias;
     private Aggregate aggregateFunc;
     private Transfert transfertFunc;
-    Neural(float weight, Aggregate aggregateFunc, Transfert transfertFunc) {
-        this.weight = weight;
+    Neural(double bias, Aggregate aggregateFunc, Transfert transfertFunc) {
+        this.bias = bias;
         this.aggregateFunc = aggregateFunc;
         this.transfertFunc = transfertFunc;
     }
