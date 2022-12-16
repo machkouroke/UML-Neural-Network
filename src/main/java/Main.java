d2c16ad8637df0f6434af90b19297a5d088b57dc
machkouroke <machkour20.mo1@gmail.com>
2022-12-16 21:07:53 +0100

    `feat` Add of Read of file from form

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,3 @@
-package lop;
-
-
 import lop.exception.DimensionMismatchException;
 import lop.neural.MLPerceptron;
 import lop.utilities.Matrix;
--- src/main/java/api/route.java
+++ src/main/java/api/route.java
@@ -1,40 +1,72 @@
 package api;
 
 import lop.exception.DimensionMismatchException;
-import lop.neural.MLPerceptron;
+import java.io.File;
+
+import lop.utilities.CsvReader;
 import lop.utilities.Matrix;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
+import javax.servlet.MultipartConfigElement;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+import static spark.Spark.*;
 
 
 public class route {
-    public static void main(String[] args) throws DimensionMismatchException {
+    public static void main(String[] args) throws DimensionMismatchException, IOException {
         port(8080);
-        double[][] x_train = {
-                {0, 0},
-                {1, 0},
-                {0, 1},
-                {1, 1}
-        };
-        double[][] y = {
-                {0}, {1}, {1}, {0}
-        };
-        MLPerceptron nn = new MLPerceptron(3, new int[]{2, 2, 1});  // 2 inputs, 2 hidden layers, 1 output
-        nn.fit(Matrix.transpose(new Matrix(x_train)), Matrix.transpose(new Matrix(y)), 50000);
-        JSONObject neural = new JSONObject(nn);
-        System.out.println(neural);
-        JSONObject jo = new JSONObject();
-        jo.put("statut", "success");
-        JSONArray ja = new JSONArray();
-        ja.put("Chien et Chat");
-        ja.put("Pokemon");
-        jo.put("data", ja);
-        get("/list-model", (request, response) -> {
+//        double[][] x_train = {
+//                {0, 0},
+//                {1, 0},
+//                {0, 1},
+//                {1, 1}
+//        };
+//        double[][] y = {
+//                {0}, {1}, {1}, {0}
+//        };
+//        MLPerceptron nn = new MLPerceptron(3, new int[]{2, 2, 1});  // 2 inputs, 2 hidden layers, 1 output
+//        nn.fit(Matrix.transpose(new Matrix(x_train)), Matrix.transpose(new Matrix(y)), 50000);
+//        SerializationUtil.serialize(
+//                nn,
+//                "model/machkour.model"
+//        );
+//        JSONObject neural = new JSONObject(nn);
+        File uploadDir = new File("upload");
+        uploadDir.mkdir(); // create the upload directory if it doesn't exist
+
+
+        get("/list-model/:userid", (request, response) -> {
             response.type("application/json");
-            return jo.toString();
+            File directoryPath = new File("model/" + request.params(":userid"));
+            JSONArray modelList = new JSONArray(Objects.requireNonNull(directoryPath.list()));
+            JSONObject res = new JSONObject();
+            res.put("statut", "success");
+            res.put("data", modelList);
+            return res.toString();
+        });
+
+        post("/create-model/:userid", (request, response) -> {
+            request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+            try (InputStream input = request.raw().getPart("data").getInputStream()) {
+                Path tempFile = Files.createTempFile(uploadDir.toPath(), "", "");
+                Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
+                CsvReader csvReader = new CsvReader(tempFile.toString());
+                Matrix x_train = csvReader.read();
+                System.out.println(x_train);
+            }
+            String modelName = request.queryParams("model-name");
+            response.type("application/json");
+            JSONObject res = new JSONObject();
+            res.put("statut", "success");
+            res.put("data", "model created");
+            return res.toString();
         });
     }
 }
